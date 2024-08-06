b8cd7c026040f6bd127d9ecabd4b47e470990900
Lokesh S <153573033+lokesh-weby@users.noreply.github.com>
2024-08-06 19:23:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://react-db-client.vercel.app');
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
   res.header('Access-Control-Allow-Headers', 'X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization');
-
+  res.header('Access-Control-Max-Age','50');
   next();
 });
 
