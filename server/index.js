e4001cb9ebfc41af91b11052f6a1e660735a8f1b
Lokesh S <153573033+lokesh-weby@users.noreply.github.com>
2024-08-06 20:11:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@
 const express=require('express');
 const mysql=require('mysql2');
 const app=express();
+const cors = require('cors');
 
-
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
